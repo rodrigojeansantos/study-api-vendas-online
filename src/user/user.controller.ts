942c0b721e450517278be2ec905b2ee55d39c970
Rodrigo_JS <rodrigojeansantos@gmail.com>
2023-10-24 15:56:15 -0300

    feat: Add funny DTOs

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto'
 
 @Controller('user')
 export class UserController {
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto){
+        return {
+            ...createUser, 
+            password: undefined
+        }
+    }
 
     @Get()
     async getAllUsers(){
