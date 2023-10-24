4fe694159bfb1c0c0135cdcfe87780d9f0b95404
Rodrigo_JS <rodrigojeansantos@gmail.com>
2023-10-24 16:24:40 -0300

    feat: Add methods GetAll and CreateOne user, Interfaces and Service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto'
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
 
+    constructor(private readonly userService: UserService){}
+
     @Post()
     async createUser(@Body() createUser: CreateUserDto){
-        return {
-            ...createUser, 
-            password: undefined
-        }
+        return this.userService.createuser(createUser)
     }
 
     @Get()
-    async getAllUsers(){
-        return JSON.stringify({test: 'abc'})
+    async getAllUsers(): Promise<User[]>{
+        return this.userService.getAllUser()
     }
 }
