5f45a6d806f76cbcb8d2f5e687d68678575ada61
Rodrigo_JS <rodrigojeansantos@gmail.com>
2023-10-24 22:11:35 -0300

    chore: Add migration for Entitys

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/createUser.dto'
+import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './interfaces/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService){}
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
 
-    @Post()
-    async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity>{
-        return this.userService.createuser(createUser)
-    }
-
-    @Get()
-    async getAllUsers(): Promise<UserEntity[]>{
-        return this.userService.getAllUser()
-    }
+  @Get()
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userService.getAllUser();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,22 +3,23 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { hash } from 'bcrypt'
 import { Repository } from 'typeorm'
 import { CreateUserDto } from './dtos/createUser.dto'
-import { UserEntity } from './interfaces/user.entity'
+import { UserEntity } from "./interfaces/user.entity"
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>
+        private readonly userRepository: Repository<UserEntity>,
     ){}
 
-    async createuser(createUserDto: CreateUserDto): Promise<UserEntity>{
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity>{
 
         const saltOrRounds = 10
         const passwordHashed = await hash(createUserDto.password, saltOrRounds)
         
         return this.userRepository.save({
             ...createUserDto, 
+            typeUser: 1,
             password: passwordHashed
         })
     }
